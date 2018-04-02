72f1b7000734635a56bfd6f0bc5c912671542392
shravnk <brendan.ode4@gmail.com>
2018-04-02 19:51:32 -0400

    Shows now saved with artist ids

--- app/controllers/show_controller.rb
+++ app/controllers/show_controller.rb
@@ -1,11 +1,14 @@
 require 'date'
 class ShowController < ApplicationController
+	get '/shows' do 
+		erb :'/shows/all'
+	end
+
 	post '/shows/new' do
 		d = params[:date]
 		t = params[:time]
 		dt = d + " " + t
-		binding.pry
-		show = Show.create(name: params[:name], price: params[:price], age: params[:age], venue_id: params[:venue_id], start_time: dt)
+		show = Show.create(name: params[:name], price: params[:price], age: params[:age], venue_id: params[:venue_id], start_time: dt, artist_id: params[:artist])
 		#can't simply pass params array because of 'captures' key
 		
 		show.save
